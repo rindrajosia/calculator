2a7fa146f7a60a4877c39e81ae2388d9b3f7dcc7
rindra josia <rrindrajosia@gmail.com>
2020-12-09 22:38:06 +0300

    delete all folders and files

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => (
-  <div className="alert alert-success">
-    Calculator.
-  </div>
-);
-
-export default App;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-import './assets/bootstrap.min.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-);
